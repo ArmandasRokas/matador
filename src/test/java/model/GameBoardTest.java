badf43e18a99076a0b646f42ed604321add3efb1
Armandas <armandas.rokas@gmail.com>
2019-01-16 11:27:57 +0100

    setSiblingSquaresTest is separated to fine-grained tests

--- src/test/java/model/GameBoardTest.java
+++ src/test/java/model/GameBoardTest.java
@@ -5,6 +5,7 @@ import controller.ChanceCardController;
 import controller.PropertyController;
 import model.square.Square;
 import model.square.property.PropertySquare;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ui.MockGUI;
 
@@ -13,43 +14,36 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GameBoardTest {
 
+    private ChanceCardController chanceCardController;
+    private PropertyController propertyController;
+    private BankruptController bankruptController;
+    private MockGUI mockGUI;
+    private GameBoard gameBoard;
+    private Square[] squares;
+
+    @BeforeEach
+    void setup() {
+        //Arrange
+        mockGUI = new MockGUI();
+        bankruptController = new BankruptController(mockGUI);
+        chanceCardController = new ChanceCardController(mockGUI);
+        propertyController = new PropertyController(mockGUI, bankruptController);
+
+        gameBoard = new GameBoard(propertyController, chanceCardController);
+
+        squares = gameBoard.getSquareList();
+    }
 
     @Test
-    void setSiblingsTest() {
-        //Arrange
-        MockGUI mockGUI = new MockGUI();
-        BankruptController bankruptController = new BankruptController(mockGUI);
-        ChanceCardController chanceCardController = new ChanceCardController(mockGUI);
-        PropertyController propertyController = new PropertyController(mockGUI, bankruptController);
-
-        GameBoard gameBoard = new GameBoard(propertyController, chanceCardController);
-
-        Square[] squares = gameBoard.getSquareList();
-
-        PropertySquare bernstoffsvej = (PropertySquare) squares[16];
-        PropertySquare[] bernstoffsvejSiblingSquares = bernstoffsvej.getSiblingsSquares();
-        PropertySquare hellerupvej = (PropertySquare) squares[18];
-        PropertySquare strandvejen = (PropertySquare) squares[19];
-
+    void setNumberOfSiblingsTest() {
         //Act
-        boolean isFirstSiblingCorrect = false;
-        boolean isSecondSiblingCorrect = false;
-        for(PropertySquare brenstoffsvejSibling: bernstoffsvejSiblingSquares){
-            if(brenstoffsvejSibling.equals(hellerupvej)){
-                isFirstSiblingCorrect = true;
-            } else if(brenstoffsvejSibling.equals(strandvejen)){
-                isSecondSiblingCorrect= true;
-
-            }
-        }
-
-
-
         boolean isNumberOfSiblingsCorrect = true;
         for(Square square: squares){
+
             if(square instanceof PropertySquare){
                 PropertySquare propertySquare = (PropertySquare) square;
                 PropertySquare[] siblingsSquares = propertySquare.getSiblingsSquares();
+
                 for(PropertySquare siblingSquare: siblingsSquares){
                     if(siblingSquare == null){
                         isNumberOfSiblingsCorrect = false;
@@ -58,15 +52,34 @@ class GameBoardTest {
             }
         }
 
-
         //Assert
         assertTrue(isNumberOfSiblingsCorrect); // all property squares has a correct number of sibling squares.
-        assertTrue(isFirstSiblingCorrect);
-        assertTrue(isSecondSiblingCorrect);
-
-
     }
 
+    @Test
+    void setCorrectSiblings() {
+        //Arrange
+        PropertySquare bernstoffsvej = (PropertySquare) squares[16];
+        PropertySquare[] bernstoffsvejSiblingSquares = bernstoffsvej.getSiblingsSquares();
+        PropertySquare hellerupvej = (PropertySquare) squares[18];
+        PropertySquare strandvejen = (PropertySquare) squares[19];
 
+        //Act
+        boolean isFirstSiblingCorrect = false;
+        boolean isSecondSiblingCorrect = false;
 
+        for(PropertySquare bernstoffsvejSibling: bernstoffsvejSiblingSquares){
+
+            if(bernstoffsvejSibling.equals(hellerupvej)){
+                isFirstSiblingCorrect = true;
+            } else if(bernstoffsvejSibling.equals(strandvejen)){
+                isSecondSiblingCorrect= true;
+
+            }
+        }
+
+        //Assert
+        assertTrue(isFirstSiblingCorrect);
+        assertTrue(isSecondSiblingCorrect);
+    }
 }
