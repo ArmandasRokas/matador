a9b859b353193a530256e6f064a30070552184ba
s185144 <armandas.rokas@gmail.com>
2019-01-23 14:18:48 +0100

    changed cup

--- src/main/java/model/Cup.java
+++ src/main/java/model/Cup.java
@@ -8,19 +8,53 @@ public class Cup {
         this.d2 = new Die(1,6);
     }
 
-    public void roll(){
-        d1.rollDie();
-        d2.rollDie();
-    }
+//    public void roll(){
+//        d1.rollDie();
+//        d2.rollDie();
+//    }
+
+//    public int getCurrentRollScore() {
+//        return getEyesDie1() + getEyesDie2();
+//    }
+
+//    public int getEyesDie1() {
+//        return d1.getEyes();
+//    }
+//    public int getEyesDie2() {
+//        return d2.getEyes();
+//    }
+
+
+    //These are made for demonstration, original methods above these
+    private int rollScoreList[] = new int[]{1, 2, 1, 30, 33, 33, 3, 2, 3, 15, 10, 29, 39, 6, 28, 8, 3, 10, 19};
+    private int doubleRolls[] = new int[]{1, 2, 3, 6,
+            8,
+            //9,
+            10,
+            //11,
+            12, 15, 17};
+    private int currTurn = 0;
+
+    public void roll() {    }
 
     public int getCurrentRollScore() {
-        return getEyesDie1() + getEyesDie2();
+        if(currTurn >= rollScoreList.length) {
+            return 0;  //Just to test if we're out of bound on our rollScoreList
+        }
+        return rollScoreList[currTurn++];
     }
 
     public int getEyesDie1() {
-        return d1.getEyes();
+        return 1;
     }
     public int getEyesDie2() {
-        return d2.getEyes();
+        for(int i = 0 ; i < doubleRolls.length ; i++) {
+            int x = doubleRolls[i];
+
+            if(currTurn == x) {
+                return 1;
+            }
+        }
+        return 2;
     }
 }
